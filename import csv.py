5a4d6fdd20a1e77fb0aa75dfc5673d373008f5eb
Stef Vandekerckhove <stef.vandekerckhove@gmail.com>
2024-11-13 14:48:17 +0100

    Update function import_csv

--- import csv.py
+++ import csv.py
@@ -1,10 +1,11 @@
 import csv 
 from First_step_collecting_data_from_a_page import create_data
-
 data = create_data()
 
-with open('data_collection.csv', 'w') as csv_file:  
-    writer = csv.writer(csv_file)
-    for key, value in data.items():
-       writer.writerow([key, value])
+def import_csv(data):
+    with open('data_collection.csv', 'w') as csv_file:  
+        writer = csv.writer(csv_file)
+        for key, value in data.items():
+            writer.writerow([key, value])
 
+import_csv(data)
